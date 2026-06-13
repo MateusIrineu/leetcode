4d70c483bab6f867e9b8c61fdc5054dbc0e0cba4
Irineu <irineu.mateus11@example.com>
2026-06-13 10:21:11 -0300

    sql/180

---
+++ sql/180_consecutive_numbers.sql
@@ -0,0 +1,11 @@
+-- LEETCODE: SQL 
+-- EXERCISE: 180
+SELECT DISTINCT atual AS ConsecutiveNums
+FROM (
+SELECT 
+LAG(num, 1) OVER(ORDER BY id ASC) AS anterior,
+num AS atual,
+LEAD(num , 1) OVER(ORDER BY id ASC) AS proximo
+FROM Logs
+) AS tabela_ajudante
+WHERE anterior = atual AND atual = proximo;
